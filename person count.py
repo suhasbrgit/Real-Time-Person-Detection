5246ea57e709114e9e0fafd8d962ed4e992c34ac
suhasbrgit <suhasbrise2025@gmail.com>
2024-11-19 21:21:00 +0530

    Add files via upload

---
+++ person count.py
@@ -0,0 +1,53 @@
+import cv2
+import pandas as pd
+from ultralytics import YOLO
+
+model = YOLO('yolov8s.pt')
+
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE:
+        colorsBGR = [x, y]
+        print(colorsBGR)
+
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+cap = cv2.VideoCapture(0)
+
+with open("coco.txt", "r") as my_file:
+    class_list = my_file.read().split("\n")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.resize(frame, (1020, 500))
+    results = model.predict(frame)
+    a = results[0].boxes.data
+    px = pd.DataFrame(a).astype("float")
+
+    person_count = 0
+
+    for index, row in px.iterrows():
+        x1 = int(row[0])
+        y1 = int(row[1])
+        x2 = int(row[2])
+        y2 = int(row[3])
+        d = int(row[5])
+        c = class_list[d]
+        if 'person' in c:
+            person_count += 1
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, str(c), (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+
+    cv2.putText(frame, f'Person count: {person_count}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("RGB", frame)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
